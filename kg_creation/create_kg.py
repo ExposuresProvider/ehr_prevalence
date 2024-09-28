fe1a18091c5c9f4f08a3a1bc5de16c038d3e8b2c
hyi <hongyi@renci.org>
2024-09-28 17:12:55 -0400

    first version of kg creation

--- kg_creation/create_kg.py
+++ kg_creation/create_kg.py
@@ -1,28 +1,33 @@
 import os
 import argparse
 import numpy as np
+from scipy.stats import chi2_contingency
 from datetime import datetime
 import pandas as pd
 from utils import normalize_nodes
 
+omop_ids_not_mapped = []
 
 def get_normalized_id_and_name(row_id, map_dict):
     mapped_data = map_dict.get(row_id, {})
+    eid = label = ''
     if mapped_data:
-        equiv_ids = [eq_id['identifier'] for eq_id in mapped_data['equivalent_identifiers']]
-        equiv_labels = [eq_id['label'] for eq_id in mapped_data['equivalent_identifiers']]
-        id_to_label = {}
-        for eid, elabel in zip(equiv_ids, equiv_labels):
-            id_to_label[eid] = elabel
-        if len(equiv_ids) <= 0 or (row_id in equiv_ids):
-            return row_id, id_to_label[row_id]
-        # if not found, return the first equivalent identifier in the list
-        return equiv_ids[0], id_to_label[equiv_ids[0]]
+        for eq_id in mapped_data['equivalent_identifiers']:
+            if not eid:
+                eid = eq_id.get('identifier', '')
+            if not label:
+                label = eq_id.get('label', '')
+            if eid and label:
+                break
+
+    if eid and label:
+        return eid, label
+
     return row_id, ''
 
 
-def log_odds(c1, c2, cp, n, replace_inf=np.inf):
-    """ Calculates the log-odds and 95% CI 
+def compute_stats(c1, c2, cp, n):
+    """ Calculates the p-value, log-odds and 95% CI
 
     Params
     ------
@@ -30,91 +35,84 @@ def log_odds(c1, c2, cp, n, replace_inf=np.inf):
     c2: count for concept 2
     cp: concept-pair count
     n: total population size
-    replace_inf: (Optional) If specified, replaces +Inf or -Inf with +replace_inf or -replace_inf (useful because JSON
-                 doesn't allow Infinity)
 
     Returns
     -------
-    (log-odds, [95% CI lower bound, 95% CI upper bound])
+    (p_value, log-odds, [95% CI lower bound, 95% CI upper bound])
     """
     a = cp
     b = c1 - cp
     c = c2 - cp
     d = n - c1 - c2 + cp
+    contingency_table = np.array([[a, b], [c, d]])
+    _, p_value, _, _ = chi2_contingency(contingency_table, correction=False)
+
     # Check b/c <= 0 since Poisson perturbation can cause b or c to be negative
     if b <= 0 or c <= 0:
         if a == 0:
-            return 0, [0, 0]
+            return p_value, 0, [0, 0]
         else:
-            return replace_inf, [replace_inf, replace_inf]
+            return p_value, np.inf, [np.inf, np.inf]
     else:
         log_odds_val = np.log((a*d)/(b*c))
         ci = 1.96 * np.sqrt(1/a + 1/b + 1/c + 1/d)
-        ci = [clip(log_odds_val - ci, replace_inf), clip(log_odds_val + ci, replace_inf)]
-        return clip(log_odds_val, replace_inf), ci
-
-
-def clip(x, clip_val):
-    """ Clip values to [-clip_val, clip_val]
-    
-    Params
-    ------
-    x: value to clip_val
-    clip_val: value to clip to
-    
-    Returns
-    -------
-    clipped value 
-    """
-    # return min(max(x, -clip), clip)
-    return -clip_val if x < -clip_val else clip_val if x > clip_val else x
+        ci = [log_odds_val - ci, log_odds_val + ci]
+        return p_value, log_odds_val, ci
 
 
 def compute_edge_info(row, map_df, total_pats):
-    # return subject, object, predicate, log_odds_ratio, log_odds_ratio_95_ci
+    # return subject, subject_name, object, object_name, predicate, chi_squared_p_value,
+    # log_odds_ratio, log_odds_ratio_95_confidence_interval, count_pair
     omop_id_1, omop_id_2, count_1, count_2, count_pair = (row['concept_id1'], row['concept_id2'],
                                                           row['count_concept_id1'], row['count_concept_id2'],
                                                           row['count_pair'])
     map_row_1 = map_df[map_df['omop_id'] == omop_id_1]
     map_row_2 = map_df[map_df['omop_id'] == omop_id_2]
     if not map_row_1.empty:
-        biolink_id_1 = map_row_1.iloc[0]['normalized_biolink_id']
+        biolink_id_1 = map_row_1.iloc[0]['normalized_biolink_id'] if map_row_1.iloc[0]['normalized_biolink_id'] \
+            else map_row_1.iloc[0]['biolink_id']
         biolink_label_1 = map_row_1.iloc[0]['normalized_biolink_label']
     else:
-        biolink_id_1 = biolink_label_1 = None
+        omop_ids_not_mapped.append(omop_id_1)
+        biolink_id_1 = biolink_label_1 = ''
     if not map_row_2.empty:
-        biolink_id_2 = map_row_2.iloc[0]['normalized_biolink_id']
+        biolink_id_2 = map_row_2.iloc[0]['normalized_biolink_id'] if map_row_2.iloc[0]['normalized_biolink_id'] \
+            else map_row_2.iloc[0]['biolink_id']
         biolink_label_2 = map_row_2.iloc[0]['normalized_biolink_label']
     else:
-        biolink_id_2 = biolink_label_2 = None
+        biolink_id_2 = biolink_label_2 = ''
+        omop_ids_not_mapped.append(omop_id_2)
 
     if count_1 < 10 or count_2 < 10 or count_pair < 10:
-        return biolink_id_1, biolink_label_1, biolink_id_2, biolink_label_2, None, None, None, None, count_pair
+        # don't return edge info if counts are less than 10 to protect patient privacy
+        return biolink_id_1, biolink_label_1, biolink_id_2, biolink_label_2, None, None, None, None
 
     # calculate log-odds
-    lo, lo_ci = log_odds(count_1, count_2, count_pair, total_pats)
-
+    p_val, lo, lo_ci = compute_stats(count_1, count_2, count_pair, total_pats)
     if lo_ci[0] > 0.5 or lo_ci[1] < -0.5:
         predicate = 'biolink:positively_correlated_with' if lo_ci[0] > 0 else 'biolink:negatively_correlated_with'
-        return biolink_id_1, biolink_label_1, biolink_id_2, biolink_label_2, predicate, lo, lo_ci[0], lo_ci[1], count_pair
+        return biolink_id_1, biolink_label_1, biolink_id_2, biolink_label_2, predicate, p_val, lo, lo_ci
     else:
-        return biolink_id_1, biolink_label_1, biolink_id_2, biolink_label_2, None, None, None, None, count_pair
+        return biolink_id_1, biolink_label_1, biolink_id_2, biolink_label_2, None, None, None, None
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process input arguments.')
     parser.add_argument('--input_concept_count_file', type=str,
-                        default='kg_creation/data/concept_counts_2018-2022_randomized_mincount-11_N-2306126_hierarchical_20240826-1228.txt',
+                        default='/projects/datatrans/CarolinaOpenHealthData/full_set_results/'
+                                'concept_counts_2018-2022_randomized_mincount-11_N-2306126_hierarchical_20240826-1228.txt',
                         help='input tsv file that includes concept counts')
     parser.add_argument('--input_concept_pair_count_file', type=str,
-                        default='kg_creation/data/concept_pair_counts_2018-2022_randomized_mincount-11_N-2306126_hierarchical_20240826-1228.txt',
+                        default='/projects/datatrans/CarolinaOpenHealthData/full_set_results/'
+                                'concept_pair_counts_2018-2022_randomized_mincount-11_N-2306126_hierarchical_20240826-1228.txt',
                         help='input tsv file that includes concept pair counts')
     parser.add_argument('--input_mapping_file', type=str,
-                        default='kg_creation/mapping/mappings.csv',
+                        # default='mapping/mappings.csv',
+                        default='/projects/datatrans/CarolinaOpenHealthData/mapping/mappings.csv',
                         help='input csv that includes mappings from OMOP concept id to biolink id')
-    parser.add_argument('--patient_num', type=int, default=1731858, help='number of total patients in the data')
+    parser.add_argument('--patient_num', type=int, default=2344578, help='number of total patients in the data')
     parser.add_argument('--output_kg_file', type=str,
-                        default='kg_creation/data/unc_omop_2018_2022_kg.csv',
+                        default='/projects/datatrans/CarolinaOpenHealthData/full_set_results/output/unc_omop_2018_2022_kg.csv',
                         help='output csv that includes knowledge graph csv created from concept count and concept pair counts')
 
     args = parser.parse_args()
@@ -125,7 +123,7 @@ if __name__ == '__main__':
     output_kg_file = args.output_kg_file
 
     t1 = datetime.now()
-    mapping_df = pd.read_csv(input_mapping_file)
+    mapping_df = pd.read_csv(input_mapping_file, dtype=str)
     if 'omop_id' not in mapping_df.columns or 'biolink_id' not in mapping_df.columns:
         print(f'two required columns: omop_id and biolink_id, are not in the mapping input file {input_mapping_file}')
         exit(1)
@@ -141,9 +139,11 @@ if __name__ == '__main__':
         mapping_df.to_csv(f'{os.path.splitext(input_mapping_file)[0]}_normalized.csv', index=False)
     print(f'mapping is loaded and normalized, time taken: {(datetime.now() - t1).seconds}')
 
-    input_concept_count_df = pd.read_csv(input_concept_count_file, sep='\t', usecols=['concept_id', 'count'])
+    input_concept_count_df = pd.read_csv(input_concept_count_file, sep='\t', usecols=['concept_id', 'count'],
+                                         dtype={'concept_id': str, 'count': int})
     input_concept_pair_count_df = pd.read_csv(input_concept_pair_count_file, sep='\t',
-                                              usecols=['concept_id1', 'concept_id2', 'count'])
+                                              usecols=['concept_id1', 'concept_id2', 'count'],
+                                              dtype={'concept_id1': str, 'concept_id2': str, 'count': int})
 
     # Perform left join on 'concept_id1'
     joined_df = pd.merge(input_concept_pair_count_df, input_concept_count_df, left_on='concept_id1',
@@ -161,7 +161,16 @@ if __name__ == '__main__':
     joined_df.drop(columns=['concept_id'], inplace=True)
 
     t1 = datetime.now()
-    joined_df[['subject', 'subject_name', 'object', 'object_name', 'predicate', 'log_odds_ratio',
-               'log_odds_ratio_95_ci_lower', 'log_odds_ratio_95_ci_lower', 'total_sample_size']] \
+    joined_df[['subject', 'subject_name', 'object', 'object_name', 'predicate', 'chi_squared_p_value', 'log_odds_ratio',
+               'log_odds_ratio_95_confidence_interval']] \
         = joined_df.apply(lambda row: compute_edge_info(row, mapping_df, patient_num), axis=1, result_type='expand')
+    joined_df.drop(columns=['concept_id1', 'concept_id2', 'count_pair', 'count_concept_id1', 'count_concept_id2'],
+                   inplace=True)
+    joined_df.to_csv(output_kg_file, index=False)
+
+    if omop_ids_not_mapped:
+        omop_ids_not_mapped = list(set(omop_ids_not_mapped))
+        with open(f'{os.path.splitext(output_kg_file)[0]}_omop_ids_not_mapped.txt', 'w') as f:
+            f.writelines([f'{line}\n' for line in omop_ids_not_mapped])
+
     print(f'knowledge graph is created, time taken: {(datetime.now() - t1).seconds}')
